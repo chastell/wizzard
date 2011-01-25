e304867166f3ceca6f979768e4bf9b4ed32b4d1e
Piotr Szotkowski <chastell@chastell.net>
2011-01-25 08:10:54 +0100

    unindentation

--- lib/wizzard/app.rb
+++ lib/wizzard/app.rb
@@ -1,32 +1,28 @@
-module Wizzard
+module Wizzard class App < Sinatra::Base
 
-  class App < Sinatra::Base
-
-    before do
-      content_type 'application/json'
-    end
-
-    get '/dicts' do
-      Aspell.list_dicts.map(&:code).uniq.to_json
-    end
-
-    get '/dicts/:dict/autocorrect' do |dict|
-      spell = Aspell.new(dict)
-      [params['text'].split.map { |word| spell.suggest(word).first }.join(' ')].to_json
-    end
-
-    get '/dicts/:dict/check' do |dict|
-      [Aspell.new(dict).list_misspelled([params['text']]).empty?].to_json
-    end
-
-    get '/dicts/:dict/errors' do |dict|
-      Aspell.new(dict).list_misspelled([params['text']]).to_json
-    end
-
-    get '/dicts/:dict/suggest' do |dict|
-      Aspell.new(dict).suggest(params['text']).to_json
-    end
-
+  before do
+    content_type 'application/json'
   end
 
-end
+  get '/dicts' do
+    Aspell.list_dicts.map(&:code).uniq.to_json
+  end
+
+  get '/dicts/:dict/autocorrect' do |dict|
+    spell = Aspell.new(dict)
+    [params['text'].split.map { |word| spell.suggest(word).first }.join(' ')].to_json
+  end
+
+  get '/dicts/:dict/check' do |dict|
+    [Aspell.new(dict).list_misspelled([params['text']]).empty?].to_json
+  end
+
+  get '/dicts/:dict/errors' do |dict|
+    Aspell.new(dict).list_misspelled([params['text']]).to_json
+  end
+
+  get '/dicts/:dict/suggest' do |dict|
+    Aspell.new(dict).suggest(params['text']).to_json
+  end
+
+end end
--- spec/wizzard/app_spec.rb
+++ spec/wizzard/app_spec.rb
@@ -1,90 +1,86 @@
 # encoding: UTF-8
 
-module Wizzard
+module Wizzard describe App do
 
-  describe App do
-
-    RSpec::Matchers.define :json_decode_to do |expected|
-      match do |actual|
-        [
-          actual.ok?,
-          actual.headers['Content-Type'] == 'application/json',
-          JSON.parse(actual.body) == expected,
-        ].all?
-      end
-      failure_message_for_should do |actual|
-        "expected #{actual.body} to JSON-decode to #{expected}"
-      end
+  RSpec::Matchers.define :json_decode_to do |expected|
+    match do |actual|
+      [
+        actual.ok?,
+        actual.headers['Content-Type'] == 'application/json',
+        JSON.parse(actual.body) == expected,
+      ].all?
     end
-
-    include Rack::Test::Methods
-
-    def app
-      App
+    failure_message_for_should do |actual|
+      "expected #{actual.body} to JSON-decode to #{expected}"
     end
+  end
 
-    context 'information queries' do
+  include Rack::Test::Methods
 
-      it 'lists the available dictionaries' do
-        get '/dicts'
-        last_response.should json_decode_to ['en', 'en_CA', 'en_GB', 'en_US']
-      end
+  def app
+    App
+  end
 
-    end
+  context 'information queries' do
 
-    context 'spelling queries' do
-
-      it 'checks a given word’s spelling according to the specified dictionary' do
-        get '/dicts/en/check?text=colour'
-        last_response.should json_decode_to [true]
-
-        get '/dicts/en_GB/check?text=colour'
-        last_response.should json_decode_to [true]
-
-        get '/dicts/en_US/check?text=colour'
-        last_response.should json_decode_to [false]
-      end
-
-      it 'checks a sentence’s spelling according to the specified dictionary' do
-        get '/dicts/en_GB/check?text=colour+of+magic'
-        last_response.should json_decode_to [true]
-
-        get '/dicts/en_US/check?text=colour+of+magic'
-        last_response.should json_decode_to [false]
-      end
-
-      it 'finds misspelled words in sentences according to the specified dictionary' do
-        get '/dicts/en/errors?text=a+sentence+wtih+smoe+errors'
-        last_response.should json_decode_to ['wtih', 'smoe']
-      end
-
-    end
-
-    context 'correcting queries' do
-
-      it 'corrects a given word’s spelling according to the specified dictionary' do
-        get '/dicts/en_GB/autocorrect?text=sanitize'
-        last_response.should json_decode_to ['sanitise']
-
-        get '/dicts/en_US/autocorrect?text=sanitise'
-        last_response.should json_decode_to ['sanitize']
-      end
-
-      it 'corrects a sentence’s spelling according to the specified dictionary' do
-        get '/dicts/en_GB/autocorrect?text=sanitised+aluminum'
-        last_response.should json_decode_to ['sanitised aluminium']
-
-        get '/dicts/en_US/autocorrect?text=sanitised+aluminum'
-        last_response.should json_decode_to ['sanitized aluminum']
-      end
-
-      it 'suggests alternative spellings for a given word' do
-        get '/dicts/en_GB/suggest?text=rewolutionary'
-        last_response.should json_decode_to ['revolutionary', 'evolutionary', 'reflationary', "revolutionary's"]
-      end
-
+    it 'lists the available dictionaries' do
+      get '/dicts'
+      last_response.should json_decode_to ['en', 'en_CA', 'en_GB', 'en_US']
     end
 
   end
 
-end
+  context 'spelling queries' do
+
+    it 'checks a given word’s spelling according to the specified dictionary' do
+      get '/dicts/en/check?text=colour'
+      last_response.should json_decode_to [true]
+
+      get '/dicts/en_GB/check?text=colour'
+      last_response.should json_decode_to [true]
+
+      get '/dicts/en_US/check?text=colour'
+      last_response.should json_decode_to [false]
+    end
+
+    it 'checks a sentence’s spelling according to the specified dictionary' do
+      get '/dicts/en_GB/check?text=colour+of+magic'
+      last_response.should json_decode_to [true]
+
+      get '/dicts/en_US/check?text=colour+of+magic'
+      last_response.should json_decode_to [false]
+    end
+
+    it 'finds misspelled words in sentences according to the specified dictionary' do
+      get '/dicts/en/errors?text=a+sentence+wtih+smoe+errors'
+      last_response.should json_decode_to ['wtih', 'smoe']
+    end
+
+  end
+
+  context 'correcting queries' do
+
+    it 'corrects a given word’s spelling according to the specified dictionary' do
+      get '/dicts/en_GB/autocorrect?text=sanitize'
+      last_response.should json_decode_to ['sanitise']
+
+      get '/dicts/en_US/autocorrect?text=sanitise'
+      last_response.should json_decode_to ['sanitize']
+    end
+
+    it 'corrects a sentence’s spelling according to the specified dictionary' do
+      get '/dicts/en_GB/autocorrect?text=sanitised+aluminum'
+      last_response.should json_decode_to ['sanitised aluminium']
+
+      get '/dicts/en_US/autocorrect?text=sanitised+aluminum'
+      last_response.should json_decode_to ['sanitized aluminum']
+    end
+
+    it 'suggests alternative spellings for a given word' do
+      get '/dicts/en_GB/suggest?text=rewolutionary'
+      last_response.should json_decode_to ['revolutionary', 'evolutionary', 'reflationary', "revolutionary's"]
+    end
+
+  end
+
+end end
